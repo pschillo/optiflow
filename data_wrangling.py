463d92288331b7622ab6551879f2cf95564de9ec
pschillo <pschillo@uni-osnabrueck.de>
2023-10-06 16:35:05 +0200

    flight data generator

--- data_wrangling.py
+++ data_wrangling.py
@@ -20,3 +20,19 @@ FAA_df = FAA_df.dropna(axis=0,subset=['Carrier Code', 'Flight Number'])
 FAA_df['Flight Number'] = FAA_df['Flight Number'].astype('int')
 
 # %%
+# create final datatrame
+
+Terminals = ['Terminal 1']
+Gates = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4']
+
+data_df = pd.DataFrame(columns=['Code', 'Departure', 'Terminal', 'Gate'])
+data_df['Code'] = FAA_df['Carrier Code'].astype(str) + FAA_df['Flight Number'].astype(str)
+data_df['Departure'] = FAA_df['Scheduled departure time']
+data_df['Terminal'] = 'Terminal 1'
+data_df['Gate'] = x
+
+# %%
+# inspect data_df
+data_df.head()
+
+# %%
